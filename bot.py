66e6248e17c18753d5cdc11cf7c9eb8a3d9854ba
SanskariWolf <tanushpurwar@gmail.com>
2023-04-09 20:13:43 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,49 @@
+import discord
+import responses
+
+
+# Send message
+async def send_message(message, user_message, is_private):
+    try:
+        response = responses.handle_response(user_message)
+        if is_private:
+            await message.author.send(response)
+        else:
+            await message.channel.send(response)
+
+    except Exception as e:
+        print(e)
+
+
+def run_discord_bot():
+    TOKEN = 'MTA5MzM5ODg2MDU2ODczNTc3NA.GKJHMd.nsLPeu0gKHrEXAi7s8Rzl1ejiPl70IRjTePuAU'
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = discord.Client(intents=intents)
+
+    @client.event
+    async def on_ready():
+        print(f'{client.user} is now running!')
+
+    @client.event
+    async def on_message(message):
+        # Make sure bot doesn't get stuck in an infinite loop
+        if message.author == client.user:
+            return
+
+        # Get data about the user
+        username = str(message.author)
+        user_message = str(message.content)
+        channel = str(message.channel)
+
+        # Debug printing
+        print(f"{username} said: '{user_message}' ({channel})")
+
+        # If the user message contains a '?' in front of the text, it becomes a private message
+        if user_message[0] == '?':
+            user_message = user_message[1:]  # [1:] Removes the '?'
+            await send_message(message, user_message, is_private=True)
+        else:
+            await send_message(message, user_message, is_private=False)
+
+    client.run(TOKEN)
